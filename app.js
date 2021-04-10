06f789ca24fa150f4b481fa4aedd81d8d7e5ec34
Ekeso <ekesolonge@gmail.com>
2021-04-10 16:38:01 +0100

    change port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const tasksRouter = require("./routes/tasks");
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.mongoDBServer, {
